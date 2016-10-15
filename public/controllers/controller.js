59e64b7511456a28f7cb451a7c39804a7fddd96d
Adrian Asagba <adrian@littleforest.co.uk>
2016-10-15 18:44:31 +0100

    setup responsive layout

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -3,11 +3,15 @@ var myApp = angular.module('myApp',['angularUtils.directives.dirPagination']);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     console.log("AppCtrl initialised");
     $scope.results = [];
+    $scope.resultsCount = 0;
+    $scope.filterBy = {field: "Title"};
 
     $scope.getHotels = function () {
         $http.get('/hotels').success(function(response) {
             console.log(response);
             $scope.results = response.Establishments;
+            $scope.resultsCount = $scope.results.length;
         });
     }
+    $scope.getHotels();
 }]);
