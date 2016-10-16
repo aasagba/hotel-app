5302d379af379159a9108c13d2bf7f9fea1bb1ea
Adrian Asagba <adrian@littleforest.co.uk>
2016-10-16 12:51:50 +0100

    star rating filter

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -5,6 +5,9 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.results = [];
     $scope.resultsCount = 0;
     $scope.filterBy = {field: "Title"};
+    $scope.query = "";
+    $scope.rating = "Choose a rating";
+    $scope.ratingVal = "";
 
     $scope.getHotels = function () {
         $http.get('/hotels').success(function(response) {
@@ -14,4 +17,29 @@ myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
         });
     }
     $scope.getHotels();
+
+    $scope.remove = function(item){
+
+        console.log(item);
+        for(var i = $scope.results.length - 1; i >= 0; i--){
+            //console.log($scope.results[i].Stars);
+            if($scope.results[i].Stars != item){
+                console.log("match: "+$scope.results[i]);
+
+                    $scope.results.splice(i,1);
+
+            }
+        }
+        console.log($scope.results);
+        console.log($scope.results.length);
+    }
+
+    $scope.search = function (row) {
+       // console.log("$scope.ratingVal: "+$scope.ratingVal);
+        //console.log("row.Stars: " + row.Stars);
+        //console.log(row.Stars == ($scope.ratingVal || ''));
+
+        return ($scope.ratingVal == "" || row.Stars == ($scope.ratingVal || '') ||
+        row.Name == ($scope.query || ''));
+    };
 }]);
